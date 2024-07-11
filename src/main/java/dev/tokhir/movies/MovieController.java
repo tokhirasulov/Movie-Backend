36697d7d66a39f4e6441c43a2f7fde0a820100d7
Tokhir Rasulov <rasulovtokhir11@gmail.com>
2024-07-11 19:14:40 +0500

    Java movie project

---
+++ src/main/java/dev/tokhir/movies/MovieController.java
@@ -0,0 +1,27 @@
+package dev.tokhir.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String id){
+        return new ResponseEntity<Optional<Movie>>(movieService.movieById(id), HttpStatus.OK);
+    }
+}
